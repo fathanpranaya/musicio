5d013580bb14a2a0de019e5258e20ca9c55ab515
Fathan <fathan@Admins-MacBook-Pro.local>
2018-03-13 18:42:20 +0900

    Init Project

---
+++ App.js
@@ -0,0 +1,58 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Button,
+  NavigatorIOS,
+  ActivityIndicator,
+  Image,
+} from 'react-native';
+import SearchPage from './SearchPage';
+
+type Props = {};
+// to disable the warning yellow box
+console.disableYellowBox = true;
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Music I/O',
+          component: SearchPage,
+        }}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  description: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#656565',
+    marginTop: 65,
+  },
+});
